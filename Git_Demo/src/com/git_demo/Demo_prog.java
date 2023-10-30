9657013e9011c909197779933d896f56caf1afcc
varshavithalrao <149048870+varshavithalrao@users.noreply.github.com>
2023-10-30 12:25:43 +0530

    by cjc

--- Git_Demo/src/com/git_demo/Demo_prog.java
+++ Git_Demo/src/com/git_demo/Demo_prog.java
@@ -9,7 +9,10 @@ public class Demo_prog
 	}
 	public void m1()
 	{
-		System.out.println("varsha");
+		System.out.println("m1-method");
 	}
-
+        public void m2()
+	{
+		System.out.println("m2-method");
+	}
 }
